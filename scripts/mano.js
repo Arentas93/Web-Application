409baa0fcbf590ea8f788587dfa104c65d65bf55
Arentas93 <66866759+Arentas93@users.noreply.github.com>
2020-09-09 10:30:23 +0300

    Add files via upload

---
+++ scripts/mano.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function dropdownMenu(){
+            var x = document.getElementById("dropdownClick");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                    /*change topnav to topnav responsive*/
+                } else {
+                    x.className = "topnav";
+                }
+            }
+
+});
